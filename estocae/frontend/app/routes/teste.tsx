6a2cabc26c969ab5ea2a64fa41219c0f2f1e7403
Diego mac <macbookpro@Macbook-Pro-2017.local>
2025-05-11 12:51:10 -0300

    new: add react to front end

---
+++ estocae/frontend/app/routes/teste.tsx
@@ -0,0 +1,10 @@
+export default function MyRouteComponent() {
+    return (
+      <div>
+        <h1>Hello World</h1>
+        <p>
+          I'm still using React Router after like 10 years.
+        </p>
+      </div>
+    );
+  }
